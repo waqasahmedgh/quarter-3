e0d63cc0f5fad60edd1dec3f5a7f66fbdbe50708
Waqas Ahmed <waqasahmed75@gmail.com>
2024-07-27 13:56:47 +0500

    assignment 1

---
+++ assignment/assignment1/class1/class1.py
@@ -0,0 +1,24 @@
+# hussain_age : int =21
+# ahmed_age : int = 6
+# kamran_age : int = 20
+
+# print("Hussain age is " , hussain_age)
+
+# print("Ahmed is " , hussain_age + ahmed_age)
+
+# # print("Kamran is " , kamran_age + hussain_age + ahmed_age)
+
+name : str = "Pakistan Zinda bad"
+print(name) # print
+print(type(name)) # type
+print(id(name)) # physcial address
+print([i for i in dir(name) if "__" not in i]) # methods and attributes
+
+
+# name:str = "Alice"
+# age:int = 30
+# city:str = "New York"
+
+# Use an f-string to create a sentence in the format: "Alice is 30 years old and lives in New York."
+
+# print(f"{name} is {age} years old and lives in the {city}")
